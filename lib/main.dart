36f8a5ec3df08aabfd4b7789c7b83405c7c68f91
CarlAmielTristanMoron <leima.mors12@gmail.com>
2022-12-29 17:26:32 +0800

    appbar update

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+          title: 'Flutter Demo Home Page'
+      ),
     );
   }
 }
@@ -72,9 +65,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: const Text("Sample Repository"),
+        leading: const Icon(Icons.account_circle),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
