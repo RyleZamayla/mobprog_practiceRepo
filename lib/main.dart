0d718fe27ced8f52fc8805bc267d28073d2acb33
Ryle_Zamayla <joshuaryle4107@gmail.com>
2022-12-29 17:37:34 +0800

    made theme into red.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(
