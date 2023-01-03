9e136af6b319d2a743f9d2db4f059bda0a9e9dbc
Ryle_Zamayla <joshuaryle4107@gmail.com>
2023-01-03 14:17:28 +0800

    for evaluation of backend function for login

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blueGrey,
       ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(
