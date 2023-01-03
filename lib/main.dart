894acd38230bef93429ee1641f600202c6c12b90
Ryle_Zamayla <joshuaryle4107@gmail.com>
2023-01-03 14:07:44 +0800

    made theme into green and changed title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.green,
       ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(
